4f174eff9a203adbb76e4aa499291494967b15a7
dinesha17 <dineshkad17@gmail.com>
2022-11-21 20:15:48 +0530

    coomit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,50 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { DashBoardComponent } from './layouts/dash-board/dash-board.component';
+import { LoginpageComponent } from './mainpagecomponent/loginpage/loginpage.component';
+import { ListComponent } from './pageslist/list/list.component';
+import { UsersComponent } from './pageslist/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:LoginpageComponent
+  },
+  //auth layouts
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+      path: '',
+      component:LoginpageComponent
+    },
+      {
+      path: 'loginpage',
+      component:LoginpageComponent
+    }]
+  },
+  //dashboard
+  {
+    path: '',
+    component: DashBoardComponent,
+    children: [{
+      path: '',
+      component:UsersComponent
+    },
+      {
+        path: 'users',
+        component:UsersComponent
+      },
+      {
+        path: 'list',
+        component:ListComponent
+
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,37 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthLayoutModule } from './layouts/auth-layout/auth-layout.module';
+import { DashBoardModule } from './layouts/dash-board/dash-board.module';
+import { HeaderComponent } from './mainpagecomponent/header/header.component';
+import { FooterComponent } from './mainpagecomponent/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './mainpagecomponent/navbar/navbar.component';
+import { DashBoardComponent } from './layouts/dash-board/dash-board.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    DashBoardComponent,
+    AuthLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+
+    
+  
   ],
+  schemas: [
+  CUSTOM_ELEMENTS_SCHEMA
+],
   providers: [],
   bootstrap: [AppComponent]
 })
